3522c02e9aef09266d23c0bfb163cd3d67d30dbc
Kristjan Fale <kristjan.fale@gmail.com>
2020-03-31 00:20:28 +0200

    Video Progress Bar, Video Volume Bar & Timestamp

--- script.js
+++ script.js
@@ -2,6 +2,7 @@ const video = document.getElementById('video');
 const play = document.getElementById('play');
 const stop = document.getElementById('stop');
 const progress = document.getElementById('progress');
+const volume = document.getElementById('volume');
 const timestamp = document.getElementById('timestamp');
 
 // Play & pause video
@@ -24,20 +25,44 @@ function updatePlayIcon() {
   }
 }
 
+// Stop video
+function stopVideo() {
+  video.currentTime = 0; // currentTime is seconds that video is playing
+  video.pause();
+}
+
 // Update progress & timestamp
 function updateProgress() {
-  return true;
+  // (time played / whole time) * 100 -> to get %
+  progress.value = (video.currentTime / video.duration) * 100;
+
+  // Get minutes
+  let mins = Math.floor(video.currentTime / 60);
+  if (mins < 10) {
+    mins = '0' + String(mins);
+  }
+
+  // Get seconds
+  let secs = Math.floor(video.currentTime % 60);
+  if (secs < 10) {
+    secs = '0' + String(secs);
+  }
+
+  timestamp.innerText = `${mins}:${secs}`;
+  // timestamp.innerHTML = `${mins
+  //   .toString()
+  //   .padStart(2, '0')}:${secs.toString().padStart(2, '0')}`;
 }
 
 // Set video time to progress
 function setVideoProgres() {
-  return true;
+  // (% * whole time) / 100 -> to get time played
+  video.currentTime = (+progress.value * video.duration) / 100;
 }
 
-// Stop video
-function stopVideo() {
-  video.currentTime = 0;
-  video.pause();
+// Set audio volume
+function setVideoVolume() {
+  video.volume = volume.value;
 }
 
 // Event Listeners
@@ -51,3 +76,4 @@ play.addEventListener('click', toggleVideoStatus);
 stop.addEventListener('click', stopVideo);
 
 progress.addEventListener('change', setVideoProgres);
+volume.addEventListener('change', setVideoVolume);
